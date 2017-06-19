b9e839fdc89393d0b8189cf7b9ff2b563c8ec667
Eduardo Lira <eduardu.silva@hotmail.com>
2017-06-19 17:39:35 -0400

    first commit

---
+++ Assets/Script/Barra_Controller.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Barra_Controller : MonoBehaviour {
+
+	public GameObject barraAnimation, vitoria, estados, mapaCompleto;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void Barrinha(){
+		barraAnimation.GetComponent<Animator>().Play("Barra_Entra");
+	}
+	public void BarrinhaSai(){
+		barraAnimation.GetComponent<Animator>().Play("Barra_Sai");
+	}
+	public void AtivarVictory(){
+		vitoria.SetActive(true);
+		//vitoria.GetComponent<Animator>().Play("Victory_Star");
+	}
+	public void completarMapa(){
+		estados.SetActive(false);
+		mapaCompleto.SetActive(true);
+
+	}
+}
---
+++ Assets/Script/Tempo.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tempo : MonoBehaviour {
+
+	private float timer = 1;
+
+	float oldTimer;
+	bool isRunning = true;
+
+	void Start ()
+	{
+		oldTimer = timer;
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		if (isRunning)
+		{
+			timer += Time.deltaTime;
+			GetComponent<Text>().text = Mathf.RoundToInt(timer).ToString() + " s";
+
+			if (timer < 0)
+				isRunning = false;
+		}
+	}
+}
